7d556f9566f055fd24ab6fabeb2a1a553cc035d3
yanglin <lin1_yang@163.com>
2019-10-02 19:36:04 +0800

    初始化仓库

---
+++ app/src/main/java/me/zhaoming/androidjourney/MainActivity.java
@@ -0,0 +1,14 @@
+package me.zhaoming.androidjourney;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
