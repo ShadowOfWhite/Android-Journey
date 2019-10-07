a2c5b040702de58224be1b9e83800da521723bed
yanglin <lin1_yang@163.com>
2019-10-07 22:51:10 +0800

    添加下拉框

--- app/src/main/java/me/zhaoming/androidjourney/MainActivity.java
+++ app/src/main/java/me/zhaoming/androidjourney/MainActivity.java
@@ -2,15 +2,18 @@ package me.zhaoming.androidjourney;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatSpinner;
 import me.zhaoming.ui.UiMarqueeActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     AppCompatButton toMarquee;
+    AppCompatSpinner spinner;
 
 
     @Override
@@ -18,9 +21,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toMarquee = findViewById(R.id.btn_marquee);
+        spinner = findViewById(R.id.spinner);
         toMarquee.setOnClickListener(this);
 
+        initSpinner();
+
     }
+
+    private void initSpinner() {
+        String[] starArray = {"水星", "金星", "地球", "火星", "木星", "土星"};
+        ArrayAdapter<String> starAdapter = new ArrayAdapter<String>(this,
+                R.layout.item_select, starArray);
+        starAdapter.setDropDownViewResource(R.layout.item_dropdown);
+        spinner.setPrompt("请选择行星");
+        spinner.setAdapter(starAdapter);
+        spinner.setSelection(0);
+
+    }
+
 
     @Override
     public void onClick(View view) {
